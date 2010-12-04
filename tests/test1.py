03f4760c7b56f95d38af552c006072ce42724c81
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-12-03 20:25:31 -0500

    simple test: parse a string

---
+++ tests/test1.py
@@ -0,0 +1,11 @@
+
+from pith import Pith
+
+p = Pith()
+l = '8 :&: int :&: TYPE :&: 1 :&: NEW :&: run :&: FIELD :&: 38 :&: int :&: TYPE :&: 1 :&: NEW :&: clone :&: FIELD :&: 291 :&: int :&: TYPE :&: 1 :&: NEW :&: gen :&: FIELD :&: 984 :&: int :&: TYPE :&: 1 :&: NEW :&: frame :&: FIELD :&: 4.21349235547 :&: float :&: TYPE :&: 1 :&: NEW :&: rmsd :&: FIELD :&: protolyze.db.results.Result :&: SOMETYPE :&: 5 :&: OBJECT'
+
+obj = p.read(l)
+
+print obj
+for k,v in obj.__dict__.iteritems():
+    print '\t', k,'=', v
